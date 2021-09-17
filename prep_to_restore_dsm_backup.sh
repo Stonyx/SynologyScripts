ac09b4e4c5b9b426af5bc4d943295b5c3552be95
Stonyx <info@stonyx.com>
2021-09-17 13:32:55 -0400

    Modified prep_to_restore_dsm_backup.sh
    Changed the logic slightly in the prep_to_restore_dsm_backup.sh file to make it more versatile.

--- prep_to_restore_dsm_backup.sh
+++ prep_to_restore_dsm_backup.sh
@@ -4,13 +4,14 @@
 declare disk_to_restore_from="/dev/sde"
 declare -a disks_to_restore_to=("/dev/sda")
 declare -a disks_to_not_use=("/dev/sdb" "/dev/sdc" "/dev/sdd")
-declare -A dsm_array_partition_map=(["/dev/md0"]="1" ["/dev/md1"]="2")
+declare -A array_partition_map=(["/dev/md0"]="1" ["/dev/md1"]="2")
 
-# Loop through the DSM arrays and make sure everything is as expected
-for dsm_array in "${!dsm_array_partition_map[@]}"
+# Loop through the DSM arrays
+declare -i make_things_pretty=1
+for array in "${!array_partition_map[@]}"
 do
   # Get the array details
-  declare array_details=$(mdadm --misc --detail "$dsm_array")
+  declare array_details=$(mdadm --misc --detail "$array")
 
   # Get the array devices count
   declare -i array_devices_count=$(echo "$array_details" | grep --extended-regex \
@@ -21,11 +22,16 @@ do
   if (( $array_devices_count < ${#disks_to_restore_to[@]} + 1 || 
     ($array_devices_count != ${#disks_to_restore_to[@]} + 1 && ${#disks_to_not_use[@]} > 0) ))
   then
-    echo "The $dsm_array raid array device count is less than the number of disks to restore" \
+    echo "The $array raid array device count is less than the number of disks to restore" \
       "to or not equal to the number of disks to restore to and disks to not use have been" \
       "specified."
-    echo "No actions will be performed."
-    exit 1
+    echo "No actions will be performed on the $array raid array."
+    if (( $make_things_pretty == 1 ))
+    then
+      echo
+      make_things_pretty=0
+    fi
+    continue
   fi
 
   # Get the array devices details
@@ -38,7 +44,7 @@ do
   for disk in "${disks[@]}"
   do
     # Get the corresponding disk partition for this array
-    declare partition="$disk${dsm_array_partition_map[$dsm_array]}"
+    declare partition="$disk${array_partition_map[$array]}"
 
     # Check if the partition is part of this array as an active device
     if echo "$array_devices_details" | grep --extended-regex \
@@ -48,10 +54,15 @@ do
       array_devices_details=$(echo "$array_devices_details" | grep --extended-regex \
         --invert-match "^ +[0-9]+ +[0-9]+ +[0-9]+ +[0-9]+ +active sync +$partition$")
     else
-      echo "The $partition partition is not an active device in the $dsm_array raid array."
+      echo "The $partition partition is not an active device in the $array raid array."
       echo "Double check the specified disk to restore from and disk(s) to restore to."
-      echo "No actions will be performed."
-      exit 1
+      echo "No actions will be performed on the $array raid array."
+      if (( $make_things_pretty == 1 ))
+      then
+        echo
+        make_things_pretty=0
+      fi
+      continue 2
     fi
   done
 
@@ -59,7 +70,7 @@ do
   for disk in "${disks_to_not_use[@]}"
   do
     # Get the corresponding disk partition for this array
-    declare partition="$disk${dsm_array_partition_map[$dsm_array]}"
+    declare partition="$disk${array_partition_map[$array]}"
 
     # Check if the partition is part of this array as a spare device
     if echo "$array_devices_details" | grep --extended-regex \
@@ -69,10 +80,15 @@ do
       array_devices_details=$(echo "$array_devices_details" | grep --extended-regex \
         --invert-match "^ +[0-9]+ +[0-9]+ +[0-9]+ +- +spare +$partition$")
     else
-      echo "The $partition partition is not a spare device in the $dsm_array raid array."
+      echo "The $partition partition is not a spare device in the $array raid array."
       echo "Double check the specified disk(s) to not use."
-      echo "No actions will be performed."
-      exit 1
+      echo "No actions will be performed on the $array raid array."
+      if (( $make_things_pretty == 1 ))
+      then
+        echo
+        make_things_pretty=0
+      fi
+      continue 2
     fi
   done
 
@@ -83,28 +99,27 @@ do
   # Check if there are any other devices left in the array devices details string
   if [[ "$array_devices_details" != "" ]]
   then
-    echo "The $dsm_array raid array has unexpected devices or device states."
-    echo "No actions will be performed."
-    exit 1
+    echo "The $array raid array has unexpected devices or device states."
+    echo "No actions will be performed on the $array raid array."
+    if (( $make_things_pretty == 1 ))
+    then
+      echo
+      make_things_pretty=0
+    fi
+    continue
   fi
-done
 
-# Loop through the DSM arrays and perform the changes
-declare -i make_things_pretty=1
-for dsm_array in "${!dsm_array_partition_map[@]}"
-do
   # Preface the output from the mdadm commands
-  echo "The prep_to_restore_dsm_backup.sh script performed the following action(s) on the" \
-    "$dsm_array raid array:"
+  echo "Performed the following actions on the $array raid array:"
 
   # Loop through the disks to not use
   for disk in "${disks_to_not_use[@]}"
   do
     # Get the corresponding disk partition for this array
-    declare partition="$disk${dsm_array_partition_map[$dsm_array]}"
+    declare partition="$disk${array_partition_map[$array]}"
 
     # Remove the partition from this array
-    mdadm --manage "$dsm_array" --remove "$partition"
+    mdadm --manage "$array" --remove "$partition"
 
     # Overwrite the partition superblock
     echo "mdadm: zeroed $partition superblock"
@@ -115,10 +130,10 @@ do
   for disk in "${disks_to_restore_to[@]}"
   do
     # Get the corresponding disk partition for this array
-    declare partition="$disk${dsm_array_partition_map[$dsm_array]}"
+    declare partition="$disk${array_partition_map[$array]}"
 
     # Remove the partition from this array
-    mdadm --manage "$dsm_array" --fail "$partition" --remove "$partition"
+    mdadm --manage "$array" --fail "$partition" --remove "$partition"
 
     # Overwrite the partition superblock
     echo "mdadm: zeroed $partition superblock"
