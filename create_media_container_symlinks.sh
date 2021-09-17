8d1a78a0d7f098157266adbaf5f8437d9ef48924
Stonyx <info@stonyx.com>
2021-09-17 19:30:17 -0400

    Minor changes and bug fix in create_media_container_symlinks.sh file

--- create_media_container_symlinks.sh
+++ create_media_container_symlinks.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Declare needed variables
-declare -a container_paths=("/volume1/docker/radarr" "/volume3/docker/sonarr")
+declare -a container_paths=("/volume3/docker/prowlarr" "/volume3/docker/radarr" \
+  "/volume3/docker/rtorrent" "/volume3/docker/sonarr")
 
 # Preface the verbose output from the ln and chown commands
-echo "The create_media_container_symlinks.sh script performed the following action(s):"
+echo "Performed the following action(s) (if any):"
 
 # Loop through the containers
 for container_path in "${container_paths[@]}"
 do
   # Check if localtime symoblic link doesn't exist for this container
-  if [[ ! -h "$conatiner_path/localtime" ]]
+  if [[ ! -h "$container_path/localtime" ]]
   then
     # Create the symlink
     echo -n "created symlink "
