69c2b3948198cadf155f25993ab3ff0ef3432c38
Stonyx <info@stonyx.com>
2021-09-22 15:20:09 -0400

    Minor changes to setup_tun.sh file

--- setup_tun.sh
+++ setup_tun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Preface the verbose output from the mkdir command and the echo statements
-echo "The setup_tun.sh script performed the following action(s):"
+echo "Performed the following action(s) (if any):"
 
 # Check if /dev/net/tun character special file doesn't exist
 if [[ ! -c "/dev/net/tun" ]]
@@ -13,13 +13,13 @@ then
   fi
 
   # Create /dev/net/tun character special file
-  echo "mknod: created character special file ‘/dev/net/tun’"
+  echo "mknod: created character special file '/dev/net/tun'"
   mknod "/dev/net/tun" c 10 200
 fi
 
 # Check if tun module isn't loaded and load it
 if ! (lsmod | grep -q "^tun\s")
 then
-  echo "insmod: inserted kernel module ‘/lib/modules/tun.ko’"
+  echo "insmod: inserted kernel module '/lib/modules/tun.ko'"
   insmod "/lib/modules/tun.ko"
 fi
